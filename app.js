b48dcd324986e96874fed2dd880aafd1320d59a0
PelumiWeb <Pelumiogundipe905@gmail.com>
2020-11-24 03:23:16 -0800

    expess ssilify

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const enforce = require('express-sslify')
+ 
 const app = express()
 
-const port = process.env.PORT || 4000
 
+const port = process.env.PORT || 4000
+app.use(enforce.HTTPS({trustProtoHeader: true}))
 app.use(express.static('public'))
 
 app.listen(port , () => {
 console.log(port)
 })
+
